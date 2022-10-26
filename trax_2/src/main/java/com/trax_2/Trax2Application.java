93a663214b9cd5dcbe9eddcf29fbe015bd4e8854
Ved Vikas <vedd2347@gmail.com>
2022-10-26 12:31:46 +0530

    trax_2 first commit upto package creation

---
+++ trax_2/src/main/java/com/trax_2/Trax2Application.java
@@ -0,0 +1,13 @@
+package com.trax_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trax2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trax2Application.class, args);
+	}
+
+}
---
+++ trax_2/src/test/java/com/trax_2/Trax2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trax_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trax2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
